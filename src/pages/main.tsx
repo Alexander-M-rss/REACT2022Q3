d0b88039ac3a8d1788372cb2e7746884c900aa92
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-03 17:37:50 +0300

    feat: add search bar to main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header, { links } from '../components/header';
+import SearchBar from '../components/searchBar';
 
 function Main() {
-  return <Header title="Main Page" links={[links.about]} />;
+  return (
+    <>
+      <Header title="Main Page" links={[links.about]} />
+      <SearchBar placeholder="Search" />
+    </>
+  );
 }
 
 export default Main;
