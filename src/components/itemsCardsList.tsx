5dcc2edf403bf28d31e2887a934fd96dfbbeac4d
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-03 22:53:04 +0300

    feat: add items cards to main page

---
+++ src/components/itemsCardsList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ItemCard from './itemCard';
+import { IItemData } from '../data/items';
+import './itemsCardsList.css';
+
+interface IItemsCardsListProps {
+  heightOffset: string;
+  baseImgUrl: string;
+  items: IItemData[];
+}
+
+function ItemsCardsList({ heightOffset, baseImgUrl, items }: IItemsCardsListProps) {
+  return (
+    <section className="items-cards-list" style={{ height: `calc(100vh - (${heightOffset}))` }}>
+      {items.map((item) => {
+        return (
+          <ItemCard
+            key={item.id}
+            title={item.title}
+            imgUrl={baseImgUrl + item.poster_path}
+            overview={item.overview}
+            popularity={item.popularity.toString()}
+            vote_average={item.vote_average.toFixed(1)}
+            vote_count={item.vote_count.toString()}
+            release_date={item.release_date}
+          />
+        );
+      })}
+    </section>
+  );
+}
+
+export default ItemsCardsList;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Header, { links } from '../components/header';
-import SearchBar from '../components/searchBar';
+import Header, { links, HEADER_HEIGHT } from '../components/header';
+import SearchBar, { SEARCH_HEIGHT } from '../components/searchBar';
+import ItemsCardsList from '../components/itemsCardsList';
+import itemsData, { BASE_IMG_URL } from '../data/items';
 
 function Main() {
   return (
     <>
       <Header title="Main Page" links={[links.about]} />
       <SearchBar placeholder="Search" />
+      <ItemsCardsList
+        items={itemsData}
+        heightOffset={`${HEADER_HEIGHT} + ${SEARCH_HEIGHT}`}
+        baseImgUrl={BASE_IMG_URL}
+      />
     </>
   );
 }
