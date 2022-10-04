480cc972814588acf94f2a911f244323e667e8b7
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-04 14:15:02 +0300

    refactor: change main page from function to class

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,18 +4,33 @@ import SearchBar, { SEARCH_HEIGHT } from '../components/searchBar';
 import ItemsCardsList from '../components/itemsCardsList';
 import itemsData, { BASE_IMG_URL } from '../data/items';
 
-function Main() {
-  return (
-    <>
-      <Header title="Main Page" links={[links.about]} />
-      <SearchBar placeholder="Search" />
-      <ItemsCardsList
-        items={itemsData}
-        heightOffset={`${HEADER_HEIGHT} + ${SEARCH_HEIGHT}`}
-        baseImgUrl={BASE_IMG_URL}
-      />
-    </>
-  );
+class Main extends React.Component {
+  constructor(props = {}) {
+    super(props);
+    this.state = { itemsData: [] };
+  }
+
+  componentDidMount = () => {
+    this.getItemsData();
+  };
+
+  getItemsData = () => {
+    this.setState({ itemsData: itemsData });
+  };
+
+  render() {
+    return (
+      <>
+        <Header title="Main Page" links={[links.about]} />
+        <SearchBar placeholder="Search" />
+        <ItemsCardsList
+          items={itemsData}
+          heightOffset={`${HEADER_HEIGHT} + ${SEARCH_HEIGHT}`}
+          baseImgUrl={BASE_IMG_URL}
+        />
+      </>
+    );
+  }
 }
 
 export default Main;
