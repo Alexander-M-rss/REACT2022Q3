44f1451400bdbb33b2b10e3a2fdf54e64acc46a9
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-04 18:48:33 +0300

    feat: add test for ItemsCardsList component

---
+++ src/components/itemsCardsList.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ItemsCardsList from './itemsCardsList';
+import itemsData, { BASE_IMG_URL } from '../data/items';
+
+describe('ItemsCardsList', () => {
+  it('renders component', () => {
+    expect(itemsData.length).toBeGreaterThan(1);
+    render(<ItemsCardsList items={itemsData} heightOffset={`70px`} baseImgUrl={BASE_IMG_URL} />);
+    expect(screen.getAllByRole('img').length).toBe(itemsData.length);
+    expect(screen.getAllByRole('heading').length).toBe(itemsData.length * 2);
+  });
+});
