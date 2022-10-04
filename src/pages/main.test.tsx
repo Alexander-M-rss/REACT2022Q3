ed40ace86d5c821d72266ea5fd10984054e4d821
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-04 22:36:33 +0300

    feat: add test for main page

---
+++ src/pages/main.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './main';
+
+describe('NotFound page', () => {
+  it('renders component', () => {
+    render(
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    );
+    expect(screen.getByTestId('header')).toBeInTheDocument();
+    expect(screen.queryAllByRole('link').length).toBeGreaterThan(0);
+    expect(screen.getByTestId('search-bar')).toBeInTheDocument();
+    expect(screen.getByTestId('items-cards-list')).toBeInTheDocument();
+  });
+});
