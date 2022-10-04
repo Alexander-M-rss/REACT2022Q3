d9520ba53f473b36c09e5781865b396afca896ac
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-04 23:31:38 +0300

    refactor: move routes to RoutesCompanent

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import RoutesComponent from './components/routes';
+
 import './App.css';
-import Main from './pages/main';
-import About from './pages/about';
-import NotFound from './pages/404';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Routes>
+        <RoutesComponent />
       </BrowserRouter>
     </div>
   );
