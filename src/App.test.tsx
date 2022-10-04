fd1ecdd0b66c6c7dc8b4c186f9ad6a8f0b00ea21
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-04 23:07:34 +0300

    feat: add test for App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders Main page by default ', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', {name: 'Main Page'})).toBeInTheDocument();
+  });
+  it('renders About Us page by link from Main page and return to Main page by link from About US', () => {
+    render(<App />);
+    userEvent.click(screen.getByText(/About Us/));
+    expect(screen.getByRole('heading', {name: 'About Us'})).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Main/));
+    expect(screen.getByRole('heading', {name: 'Main Page'})).toBeInTheDocument();
+  });
 });
