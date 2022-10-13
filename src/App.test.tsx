5b93e1ffa417d14d72f9cf15a7831be806499c59
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-13 21:38:05 +0300

    feat: add forms page to App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,13 +6,21 @@ import userEvent from '@testing-library/user-event';
 describe('App', () => {
   it('renders Main page by default ', () => {
     render(<App />);
-    expect(screen.getByRole('heading', {name: 'Main Page'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Main Page' })).toBeInTheDocument();
   });
   it('renders About Us page by link from Main page and return to Main page by link from About US', () => {
     render(<App />);
     userEvent.click(screen.getByText(/About Us/));
-    expect(screen.getByRole('heading', {name: 'About Us'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'About Us' })).toBeInTheDocument();
     userEvent.click(screen.getByText(/Main/));
-    expect(screen.getByRole('heading', {name: 'Main Page'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Main Page' })).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Forms/));
+    expect(screen.getByRole('heading', { name: 'Forms Page' })).toBeInTheDocument();
+    userEvent.click(screen.getByText(/About/));
+    expect(screen.getByRole('heading', { name: 'About Us' })).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Forms/));
+    expect(screen.getByRole('heading', { name: 'Forms Page' })).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Main/));
+    expect(screen.getByRole('heading', { name: 'Main Page' })).toBeInTheDocument();
   });
 });
