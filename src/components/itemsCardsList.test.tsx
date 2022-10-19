eb040bbb9297669bc8ad44f76e7598e4b61362df
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-10-19 17:22:38 +0300

    feat: add more tests for ItemCard and ItemsCardsList components

--- src/components/itemsCardsList.test.tsx
+++ src/components/itemsCardsList.test.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ItemsCardsList from './itemsCardsList';
 import itemsData from '../data/items';
+import userEvent from '@testing-library/user-event';
+
+const onClickHendler = jest.fn();
 
 describe('ItemsCardsList', () => {
   it('renders component', () => {
-    expect(itemsData.length).toBeGreaterThan(1);
-    render(<ItemsCardsList items={itemsData} />);
+    render(<ItemsCardsList items={itemsData} onClick={onClickHendler} />);
     expect(screen.getAllByRole('heading').length).toBe(itemsData.length);
+  });
+  it('calls onClick handler', () => {
+    render(<ItemsCardsList items={itemsData} onClick={onClickHendler} />);
+    const names = screen.getAllByRole('heading');
+    names.forEach((el) => userEvent.click(el));
+    expect(onClickHendler).toBeCalledTimes(names.length);
   });
 });
