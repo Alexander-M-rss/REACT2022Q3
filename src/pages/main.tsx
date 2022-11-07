67db2e0a157e4ef2e22f4c22051edce3cc53f31c
Alexander-M-rss <oleksandr.m.rss@gmail.com>
2022-11-07 22:11:13 +0200

    refactor: move api request to thunk

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,37 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Header, { links } from '../components/header';
 import SearchBar from '../components/searchBar';
 import ItemsCardsList from '../components/itemsCardsList';
-import { getItems, IGetItemsOptions } from '../api/api';
+import { IGetItemsOptions } from '../api/api';
 import DownloadIndicator from 'components/downloadIndicator';
 import SortingSwitcher from 'components/sortingSwitcher';
 import PaginationSwitcher from 'components/paginationSwitcher';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
-import { saveItemsData, setDetailsIdx } from 'store/stateSlice';
+import { getItemsThunk, setDetailsIdx } from 'store/stateSlice';
 
 function Main() {
-  const [isDataLoading, setIsDataLoading] = useState(false);
   const dispatch = useAppDispatch();
   const state = useAppSelector((store) => store.state);
   const navigate = useNavigate();
 
   const getItemsData = useCallback(
     async (opt: IGetItemsOptions) => {
-      setIsDataLoading(true);
-      const data = await getItems(opt);
-      setIsDataLoading(false);
-      dispatch(
-        saveItemsData({
-          itemsData: data.items,
-          errMsg: data.errMsg,
-          pages: data.pages,
-          search: opt.search,
-          sorting: opt.sorting,
-          page: opt.page,
-          itemsPerPage: opt.limit,
-        })
-      );
+      dispatch(getItemsThunk(opt));
     },
     [dispatch]
   );
@@ -43,14 +29,23 @@ function Main() {
 
   useEffect(() => {
     if (!state.itemsData || (!state.itemsData.length && !state.errMsg.length)) {
-      getItemsData({
-        search: '',
-        sorting: state.sorting,
-        page: state.page,
-        limit: state.itemsPerPage,
-      });
+      dispatch(
+        getItemsThunk({
+          search: '',
+          sorting: state.sorting,
+          page: state.page,
+          limit: state.itemsPerPage,
+        })
+      );
     }
-  }, [getItemsData, state]);
+  }, [
+    dispatch,
+    state.errMsg.length,
+    state.itemsData,
+    state.itemsPerPage,
+    state.page,
+    state.sorting,
+  ]);
 
   return (
     <>
@@ -68,8 +63,8 @@ function Main() {
         <SortingSwitcher searchHandler={getItemsData} />
         <PaginationSwitcher searchHandler={getItemsData} />
       </div>
-      {isDataLoading && <DownloadIndicator />}
-      {!isDataLoading && (
+      {state.isDataLoading && <DownloadIndicator />}
+      {!state.isDataLoading && (
         <ItemsCardsList items={state.itemsData || []} errMsg={state.errMsg} onClick={showDetails} />
       )}
     </>
--- src/store/stateSlice.ts
+++ src/store/stateSlice.ts
@@ -1,7 +1,14 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IPersonCardProps } from 'components/personCard';
 import { IFormValues } from 'components/formTypes';
-import { IItemData, SORTING, DEFAULT_PER_PAGE } from 'api/api';
+import {
+  IItemData,
+  SORTING,
+  DEFAULT_PER_PAGE,
+  IGetItemsOptions,
+  getItems,
+  IItemsResp,
+} from 'api/api';
 
 interface IItemsPayload {
   itemsData: IItemData[];
@@ -19,6 +26,7 @@ export interface IState {
   personCards: IPersonCardProps[];
   formValues?: FormValuesState;
   itemsData?: IItemData[];
+  isDataLoading: boolean;
   errMsg: string;
   search: string;
   pages: number;
@@ -30,6 +38,7 @@ export interface IState {
 
 export const initialState: IState = {
   personCards: [],
+  isDataLoading: true,
   errMsg: '',
   search: '',
   pages: 1,
@@ -38,6 +47,31 @@ export const initialState: IState = {
   page: 1,
   detailsIdx: -1,
 };
+
+export interface IGetItemsThunkData extends IItemsResp {
+  search: string;
+  sorting: SORTING;
+  page: number;
+  itemsPerPage: number;
+}
+
+export const getItemsThunk = createAsyncThunk<
+  IGetItemsThunkData,
+  IGetItemsOptions,
+  { rejectValue: string }
+>('state/getItemsThunk', async function (opt) {
+  const resp = await getItems(opt);
+
+  return {
+    items: resp.items,
+    pages: resp.pages,
+    errMsg: resp.errMsg,
+    search: opt.search,
+    sorting: opt.sorting,
+    page: opt.page,
+    itemsPerPage: opt.limit,
+  };
+});
 
 const stateSlice = createSlice({
   name: 'state',
@@ -61,6 +95,25 @@ const stateSlice = createSlice({
     resetState() {
       return initialState;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getItemsThunk.pending, (state) => {
+        state.isDataLoading = true;
+        state.errMsg = '';
+      })
+      .addCase(getItemsThunk.fulfilled, (state, action) => {
+        const { items, errMsg, pages, search, sorting, page, itemsPerPage } = action.payload;
+
+        state.isDataLoading = false;
+        state.itemsData = items;
+        state.errMsg = errMsg;
+        state.pages = pages;
+        state.search = search;
+        state.sorting = sorting;
+        state.page = page;
+        state.itemsPerPage = itemsPerPage;
+      });
   },
 });
 
